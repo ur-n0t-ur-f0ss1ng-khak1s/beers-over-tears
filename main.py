2cfd142b8e3c2da5dea1510d583f95d0b51807fe
ur-n0t-ur-f0ss1ng-khak1s <jackwinford@gmail.com>
2024-06-18 16:01:14 -0800

    adjusted text box

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ is_fullscreen = False
 # Define colors
 WHITE = (255, 255, 255)
 BLACK = (0, 0, 0)
+PALE_BLUE = (173, 216, 230)  # RGB values for pale blue
 
 # Initialize Pygame
 pygame.init()
@@ -18,7 +19,7 @@ pygame.display.set_caption('beers over tears')
 font = pygame.font.Font(None, 36)  # None uses the default font, 36 is the font size
 
 # Render the text
-text_surface = font.render('Hello, Pygame!', True, BLACK)  # True for anti-aliasing
+text_surface = font.render('Hello, Pygame!', True, WHITE)  # True for anti-aliasing
 
 # Load left human art
 #image_path = './art/ross.png'
@@ -27,7 +28,7 @@ scaled__left_image = pygame.transform.scale(left_image, (left_image.get_width() 
 
 # Load background
 #image_path = './art/candle-lit-beers.jpeg'
-back_image = pygame.image.load('./art/candle-lit-beers.jpeg')
+back_image = pygame.image.load('./art/warning-dangerous-waters-and-mud-flats.jpeg')
 
 def adjust_to_aspect_ratio(width, height):
     # Maintain a 4:3 aspect ratio
@@ -44,15 +45,15 @@ def scale(w,h):
     width, height = adjust_to_aspect_ratio(w, h)
     print(f"{width},{height}")
 
-    text_rect = text_surface.get_rect(center=(w // 2, ((h - height) / 2) + 10))
-
+    # scaling the left character
     scaled_left_image = pygame.transform.scale(left_image, (left_image.get_width() * width/200, left_image.get_height() * height/150))
-    # Get the rectangle for positioning
-    left_per_rect = scaled_left_image.get_rect()
+
+    left_per_rect = scaled_left_image.get_rect() # rectangle for positioning on the screen
     left_per_rect.topleft = (((w - width) / 2) + 20, ((h - height) / 2)+(height-(79*(height/150))))
 
+    # scaling the background
     back_image = pygame.transform.scale(back_image, (back_image.get_width() * width/back_image.get_width(), back_image.get_height() * height/back_image.get_height()))
-    # Get the rectangle for positioning
+
     back_rect = back_image.get_rect()
     back_rect.topleft = ((w - width) / 2,(h - height) / 2)
 
@@ -78,13 +79,25 @@ while True:
 
     
     # Fill the screen with blue 
-    screen.fill((0, 0, 255))
+    screen.fill(PALE_BLUE)
 
     # Render the background 
     screen.blit(back_image, back_rect)
 
+    outer_rect = pygame.Rect(20, 10, 760, 280)
+    pygame.draw.rect(screen, PALE_BLUE, outer_rect)  # x, y, width, height
+
     # Render the image
     screen.blit(scaled_left_image, left_per_rect)
+
+     # Calculate the inner text rectangle dimensions and position
+    inner_rect = pygame.Rect(
+        outer_rect.left + 10,
+        outer_rect.top + 10,
+        outer_rect.width - 2 * 10,
+        outer_rect.height - 2 * 10
+    )
+    text_rect = text_surface.get_rect(center=inner_rect.center)
 
     # Render the text surface
     screen.blit(text_surface, text_rect)
