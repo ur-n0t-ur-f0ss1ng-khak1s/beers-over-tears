ece7411246e3d96e3aa279a366ac511d2743a886
ur-n0t-ur-f0ss1ng-khak1s <jackwinford@gmail.com>
2024-07-03 16:07:50 -0800

    new art and charitable intro 1 is written

---
+++ intro_charitable1.py
@@ -0,0 +1,13 @@
+# intro_charitable1.py
+intro_charitable1 = [
+    {"text": "Let me take a look underneath. Ah ha your drive shaft and your spark box just need to be replaced. Also, I'm going to have remove the catalytic converter.", "speaker": 1},
+    {"text": "Sounds good to me! I sure don't know much about cars so I'm not much use here. Now, if we were fixing the broken federal government I know exactly what I'd do. Governments are much more simple than cars.", "speaker": 3}, 
+    {"text": "Oh really!? \n \n (1) Tell me more about your political opinions \n \n (2) It would take ChatGPT a few hours to reform the federal government into a utopia. If I want help with something easy like politics I'll ask artificial intelligence and you can spare us your \"Acapulco Gold\" ignorance", "speaker": 2, "input": {1: {"victory": "charitable", "VP": 10}, 2: {"victory": "selfish", "VP": 15}}},
+    {"text": "I'm glad you're eager to learn. Not everyone in your generation is so open minded. Anyway, first I would make fractional reserve banking punishable by death. The Senate would have random appointments for all members, just like a jury. Finally, I would declare war against Mongolia and I'm not afraid to use weapons of mass destruction.", "speaker": 3},
+    {"text": "....", "speaker": 3},
+    {"text": ".... thank you ....", "speaker": 0},
+    {"text": "Alrighty there **bleeehp** yer good to go. Just take 'er a bit easier than normal on the curves", "speaker": 0},
+    {"text": "Thank you Jesus and thank you Mother Mary Jane!! you two are truly some rastafarian angels. I owe you my life.", "speaker": 3},
+    {"text": "Well a life debt would be nice but for now how about you drop us off at the liquor store?", "speaker": 0},
+    {"text": "The quest continues, to the liquor store! huzzah", "speaker": 3},
+]
